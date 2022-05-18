96501f968766e0df8ab7a5512b70c33f37f373ea
TA2LSM <semihsenol@gmail.com>
2022-05-18 16:34:13 +0300

    first commit

---
+++ vidly/src/App.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+import Navbar from './components/navbar';
+import Home from './components/home';
+import Products from './components/products';
+import Dashboard from './components/admin/dashboard';
+import Posts from './components/posts';
+import ProductDetails from './components/productDetails';
+import NotFound from './components/notFound';
+
+import Movies from './components/movies';
+
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <main className='container position-absolute'>
+        <div
+          className='row-sm mb-2'
+          style={{
+            height: '50px',
+            backgroundColor: 'aqua',
+            border: '3px solid green',
+            fontSize: '20px',
+            textAlign: 'center',
+          }}
+        >
+          <p style={{ marginTop: '5px' }}>
+            <strong>
+              <i>Vidly </i>
+            </strong>
+            Application (Modified by:{' '}
+            <strong>
+              <a
+                target='_blank'
+                href='https://github.com/TA2LSM'
+              >
+                TA2LSM
+              </a>
+            </strong>
+            )
+          </p>
+        </div>
+
+        <Navbar />
+        <div className='content'>
+          <Routes>
+            <Route
+              path='/'
+              element={<Home />}
+            />
+            <Route
+              path='/products'
+              element={<Products />}
+            />
+            <Route
+              path='/posts'
+              element={<Posts />}
+            />
+            <Route
+              path='/admin'
+              element={<Dashboard />}
+            />
+            <Route
+              path='/movies'
+              element={<Movies />}
+            />
+          </Routes>
+        </div>
+      </main>
+    );
+  }
+}
+
+export default App;
---
+++ vidly/src/components/moviesTable.jsx
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+//import React from "react"; //imr kısayolu
+
+//components
+import Like from './common/like';
+import Table from './common/table';
+
+class MoviesTable extends Component {
+  columns = [
+    { path: 'title', label: 'Title', sortable: 'true' },
+    { path: 'genre.name', label: 'Genre', sortable: 'true' },
+    { path: 'numberInStock', label: 'Stock', sortable: 'true' },
+    { path: 'dailyRentalRate', label: 'Rate', sortable: 'true' },
+    {
+      label: 'Like',
+      key: 'Like',
+      sortable: 'false',
+      content: movie => (
+        <Like
+          liked={movie.liked}
+          onClick={() => this.props.onLike(movie)}
+        />
+      ),
+    },
+    {
+      label: 'Modify',
+      key: 'Modify',
+      sortable: 'false',
+      content: movie => (
+        <button
+          onClick={() => this.props.onDelete(movie)}
+          className='btn btn-danger btn-sm fw-bold'
+        >
+          Delete
+        </button>
+      ),
+    },
+  ];
+
+  render() {
+    const { movies, sortColumn, onSort } = this.props;
+
+    return (
+      <div>
+        <Table
+          columns={this.columns}
+          sortColumn={sortColumn}
+          onSort={onSort}
+          data={movies}
+        />
+      </div>
+    );
+  }
+}
+
+export default MoviesTable;
